3c6ca03c779ccaa682bd9214ee41b5b42d0b5b01
Deepak Kumar <93462418+Deepakk-kumar1@users.noreply.github.com>
2024-10-17 20:23:05 +0530

    Direct upload
    Direct upload Abhi QR Generator Project by Deepak Kumar

---
+++ navigation.js
@@ -0,0 +1,9 @@
+// Open the full-screen navbar
+function openNavbar() {
+  document.getElementById("deepak-navbar").style.width = "100%";
+}
+
+// Close the full-screen navbar
+function closeNavbar() {
+  document.getElementById("deepak-navbar").style.width = "0";
+}
