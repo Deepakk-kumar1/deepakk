eedc5ace17a9147741f4cb406c8e4a0aa2694893
Deepak Kumar <93462418+Deepakk-kumar1@users.noreply.github.com>
2025-01-28 04:48:17 +0530

    typed.jsx

---
+++ typed.jsx
@@ -0,0 +1,12 @@
+var options = {
+      strings: [
+        "अंतः अस्ति प्रारंभ", 
+        "The End is The Beginning", 
+        "अंत ही प्रारम्भ है।"
+      ],
+      typeSpeed: 50, // Speed of typing
+      backSpeed: 30, // Speed of deleting text
+      loop: true // Loop the animation
+    };
+
+    var typed = new Typed("#typing-effect", options);
