8996e44e9466bb9fc481158a4bdee40e2e6512bb
Deepak Kumar <93462418+Deepakk-kumar1@users.noreply.github.com>
2025-01-28 12:23:30 +0530

    sendmail.php

---
+++ sendmail.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "deepakk8373@gmail.com";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+    $fullMessage = "<strong>Name:</strong> $name<br>";
+    $fullMessage .= "<strong>Email:</strong> $email<br>";
+    $fullMessage .= "<strong>Subject:</strong> $subject<br>";
+    $fullMessage .= "<strong>Message:</strong><br>$message<br>";
+
+    if (mail($to, $subject, $fullMessage, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message. Please try again.";
+    }
+}
+?>
